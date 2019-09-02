c12ed3780109018e62f7bb317f9c758069c94234
2170210 <2170210@2170210S>
2019-09-02 11:55:01 +0900

    Obj2D変更

--- 2019SIC/Obj2d.cpp
+++ 2019SIC/Obj2d.cpp
@@ -4,15 +4,25 @@
 
 void OBJ2D::clear()
 {
-	ZeroMemory(SpriteHnadle, sizeof(SpriteHnadle));
+	ZeroMemory(SpriteHandle, sizeof(SpriteHandle));
 	position = VECTOR2D::VGet2D(0, 0);
 	velocity = VECTOR2D::VGet2D(0, 0);
+	size = VECTOR2D::VGet2D(0, 0);
 	SpriteNumber = 0;
 	alive = false;
 	MoveAlg = nullptr;
 }
 
+void OBJ2D::LoadSprite(char*FileName, OBJ2D::STATE StateNum, int SpriteNum)
+{
+	SpriteHandle[StateNum][SpriteNum]=LoadGraph(FileName);
+}
+
+
 void OBJ2D::draw()
 {
-	DrawGraph(position.x, position.y, SpriteHnadle[SpriteNumber], TRUE);
+	DrawGraph(position.x-size.x/2,
+		position.y-size.y/2,
+		SpriteHandle[state][SpriteNumber],
+		TRUE);
 }
--- 2019SIC/Obj2d.h
+++ 2019SIC/Obj2d.h
@@ -4,13 +4,32 @@
 
 class OBJ2D
 {
+
+
+
 public:
-	int SpriteHnadle[SPRITE_NUM];
+
+	enum STATE
+	{
+		RWAIT,
+		LWAIT,
+		RRUN,
+		LRUN,
+		RJUMP,
+		LJUMP,
+		NUM,
+	};
+
+
+	int SpriteHandle[STATE::NUM][SPRITE_NUM];
 	int SpriteNumber;
+	int state;
 	VECTOR2D  position;
 	VECTOR2D  velocity;
+	VECTOR2D  size;
 	bool alive;
 
+	void LoadSprite(char*FileName,STATE StateNum,int SpriteNum);
 	void clear();
 	void (*MoveAlg)(OBJ2D obj);
 	void draw();
