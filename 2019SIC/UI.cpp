05c1453cad6ff1c67f5fe2b19a2adee40739b606
K-Keito <54610490+K-Keito@users.noreply.github.com>
2019-09-05 01:40:21 +0900

    仕様

---
+++ 2019SIC/UI.cpp
@@ -0,0 +1,24 @@
+/*
+SceneManagerのモードによって表示するUIを変更する
+TAITLEのときはゲームの開始と終了のセレクト
+ゲーム開始でSTATEをGAMEに、終了でゲーム終了。
+
+GAMEのときは
+
+OVERのときはスコアを表示、60秒カウントか決定ボタンでTAITLEに戻る
+
+
+TAITLE,GAME,OVERの三種類の更新を作る。
+*/
+
+#include <Windows.h>
+#include "DxLib.h"
+
+#include "winmain.h"
+#include "UI.h"
+
+void OTE(){
+
+
+
+}
